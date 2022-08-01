48564aabc47e11a8eefcefc4373958a4ea56d330
RainierDotulong <rainierkd@gmail.com>
2022-08-01 16:15:00 -0700

    second

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,13 @@
-const router = require('express').Router();
-const categoryRoutes = require('./category-routes');
-const productRoutes = require('./product-routes');
-const tagRoutes = require('./tag-routes');
+const router = require('express').Router()
+const categoryRoutes = require('./category-routes')
+const productRoutes = require('./product-routes')
+const tagRoutes = require('./tag-routes')
 
-router.use('/categories', categoryRoutes);
-router.use('/products', productRoutes);
-router.use('/tags', tagRoutes);
+router.get('/', (req, res) => {
+	res.send('routing!')
+})
+router.use('/categories', categoryRoutes)
+router.use('/products', productRoutes)
+router.use('/tags', tagRoutes)
 
-module.exports = router;
+module.exports = router
